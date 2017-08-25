50afd527e96f0f4dea54867417add1ee5d9f0aa0
jiaoxn <nihaojxn@users.noreply.github.com>
2017-08-25 12:52:39 +0800

    1、修改简单选择排序文件名（direct_selection_sort.py -> simple_selection_sort.py）。

--- SelectionSort/__init__.py
+++ SelectionSort/__init__.py
@@ -1,4 +1,4 @@
-from direct_selection_sort import direct_selection_sort
+from simple_selection_sort import direct_selection_sort
 from heap_sort import heap_sort
 
 __all__ = ['direct_selection_sort', 'heap_sort']
