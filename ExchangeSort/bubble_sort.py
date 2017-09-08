8b12aeef038ed1720c3d6443de4282915fd5a136
jiaoxn <31320415+jiaoxn@users.noreply.github.com>
2017-09-08 09:18:13 +0800

    1、修改冒泡排序实现方式。

--- ExchangeSort/bubble_sort.py
+++ ExchangeSort/bubble_sort.py
@@ -18,9 +18,15 @@ def bubble_sort(lists):
     length = len(lists)
 
     for i in range(0, length):
+        did_swap = False
+
         for j in range(0, length - 1 - i):
             if lists[j] > lists[j + 1]:
                 lists[j], lists[j + 1] = lists[j + 1], lists[j]
+                did_swap = True
+
+        if did_swap is False:
+            return
 
 
 if __name__ == '__main__':
